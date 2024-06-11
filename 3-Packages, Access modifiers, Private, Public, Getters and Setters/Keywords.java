e345b3700013df653bb9f3010e6aacfe1d7ad0f3
Tejas Jogdand <tejasjogdand37@gmail.com>
2024-06-11 23:27:51 +0530

    this keyword

--- 3-Packages, Access modifiers, Private, Public, Getters and Setters/Keywords.java
+++ 3-Packages, Access modifiers, Private, Public, Getters and Setters/Keywords.java
@@ -1,3 +1,16 @@
+class A{
+    String name;
+    public void setName(String name){
+        this.name = name;
+    }
+    public void printName(){
+        System.out.println("My name is "+name);
+    }
+}
 public class Keywords {
-    
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.setName("Tejas");
+        obj.printName();
+    }
 }
